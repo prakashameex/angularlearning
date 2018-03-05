e676b2ff38385dbade48675cdbe3bb78357f083e
kamal.prakash@ameexusa.com <kamal.prakash@ameexusa.com>
2018-03-05 23:25:11 +0530

    angular file added

---
+++ my-app/src/app/heroes/heroes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+test="sample website 3";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
