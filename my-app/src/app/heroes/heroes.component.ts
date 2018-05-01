0623d006765ae945acc2f712ee494df896708af2
kamal.prakash@ameexusa.com <kamal.prakash@ameexusa.com>
2018-05-01 08:38:19 +0530

    hero component change

--- my-app/src/app/heroes/heroes.component.ts
+++ my-app/src/app/heroes/heroes.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
-  selector: 'app-heroe',
+  selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-test="sample website 3";
+
+heroes = HEROES;
   constructor() { }
 
   ngOnInit() {
